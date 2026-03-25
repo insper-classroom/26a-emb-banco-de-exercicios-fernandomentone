b9945d6c6926cde0fbf3852310ad6fa03ebcbece
fernandomentone <fernandomm9@al.insper.edu.br>
2026-03-25 10:31:10 -0300

    seja o que deus quiser

--- pisca-led-dois-botoes/main.c
+++ pisca-led-dois-botoes/main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 #include "hardware/gpio.h"
 #include "hardware/timer.h"
 #include "pico/stdlib.h"
@@ -17,40 +16,29 @@ volatile int flag_timer_g = 0;
 volatile int flag_btn_y = 0;
 volatile int flag_btn_g = 0;
 
-static bool led_g_active = false;
-static bool led_y_active = false;
-
-volatile int flag_alarm_g = 0;
 volatile int flag_alarm_y = 0;
-static alarm_id_t alarm_g;
+volatile int flag_alarm_g = 0;
 static alarm_id_t alarm_y;
+static alarm_id_t alarm_g;
 
-
-bool timer_callback_y(struct repeating_timer *t) {
-    flag_timer_y = 1;
-    return true;
-}
+static bool led_y_active = false;
+static bool led_g_active = false;
 
 bool timer_callback_g(struct repeating_timer *t) {
     flag_timer_g = 1;
     return true;
 }
 
+bool timer_callback_y(struct repeating_timer *t) {
+    flag_timer_y = 1;
+    return true;
+}
 
 void btn_callback(uint gpio, uint32_t events) {
     if (events & GPIO_IRQ_EDGE_FALL) {
-        if (gpio == BTN_Y) {
-            flag_btn_y = 1;
-        } else if ( gpio == BTN_G) {
-            flag_btn_g = 1;
-        }
-
+        if (gpio == BTN_G) flag_btn_g = 1;
+        else if (gpio == BTN_Y) flag_btn_y = 1;
     }
-}
-
-int64_t alarm_callback_y(alarm_id_t id, void *user_data) {
-    flag_alarm_y = 1;
-    return 0;
 }
 
 int64_t alarm_callback_g(alarm_id_t id, void *user_data) {
@@ -58,38 +46,38 @@ int64_t alarm_callback_g(alarm_id_t id, void *user_data) {
     return 0;
 }
 
+int64_t alarm_callback_y(alarm_id_t id, void *user_data) {
+    flag_alarm_y = 1;
+    return 0;
+}
 
 int main() {
-
-    gpio_init(BTN_Y);
-    gpio_set_dir(BTN_Y, GPIO_IN);
-    gpio_pull_up(BTN_Y);
+    stdio_init_all();
 
     gpio_init(BTN_G);
     gpio_set_dir(BTN_G, GPIO_IN);
     gpio_pull_up(BTN_G);
 
-    gpio_init(LED_Y);
-    gpio_set_dir(LED_Y, GPIO_OUT);
+    gpio_init(BTN_Y);
+    gpio_set_dir(BTN_Y, GPIO_IN);
+    gpio_pull_up(BTN_Y);
 
     gpio_init(LED_G);
     gpio_set_dir(LED_G, GPIO_OUT);
 
-    gpio_set_irq_enabled_with_callback(BTN_Y, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
-    gpio_set_irq_enabled(BTN_G, GPIO_IRQ_EDGE_FALL, true);
+    gpio_init(LED_Y);
+    gpio_set_dir(LED_Y, GPIO_OUT);
 
-    stdio_init_all();
-
-    add_repeating_timer_ms(100, timer_callback_g, NULL, &timer_g);
-    add_repeating_timer_ms(250, timer_callback_y, NULL, &timer_y);
+    gpio_set_irq_enabled_with_callback(BTN_G, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+    gpio_set_irq_enabled(BTN_Y, GPIO_IRQ_EDGE_FALL, true);
 
     while (1) {
         if (flag_btn_g) {
             flag_btn_g = 0;
             if (!led_g_active) {
                 led_g_active = true;
-                gpio_put(LED_G, 1);
                 alarm_g = add_alarm_in_ms(1000, alarm_callback_g, NULL, false);
+                add_repeating_timer_ms(100, timer_callback_g, NULL, &timer_g);
             }
         }
 
@@ -97,8 +85,8 @@ int main() {
             flag_btn_y = 0;
             if (!led_y_active) {
                 led_y_active = true;
-                gpio_put(LED_Y, 1);
                 alarm_y = add_alarm_in_ms(2000, alarm_callback_y, NULL, false);
+                add_repeating_timer_ms(250, timer_callback_y, NULL, &timer_y);
             }
         }
 
@@ -107,8 +95,12 @@ int main() {
             flag_alarm_y = 0;
             cancel_alarm(alarm_g);
             cancel_alarm(alarm_y);
+            cancel_repeating_timer(&timer_g);
+            cancel_repeating_timer(&timer_y);
             led_g_active = false;
             led_y_active = false;
+            flag_timer_g = 0;
+            flag_timer_y = 0;
             gpio_put(LED_G, 0);
             gpio_put(LED_Y, 0);
         }
@@ -127,4 +119,6 @@ int main() {
             }
         }
     }
+
+    return 0;
 }
